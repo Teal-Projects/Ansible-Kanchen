d47a83bdfb722e19b3f9c3b6d6aa76e8e8bdd2eb
Kanchen Monnin <kanchen.monnin@neofonie.de>
2020-07-01 12:19:11 +0200

    Initial Commit

--- roles/internal/requirements.sh
+++ roles/internal/requirements.sh
@@ -1,9 +1,3 @@
 #!/bin/bash                                                                                                                                                                                                      
 
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-docker.git
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-flannel.git
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-etcd.git
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-kube_master.git
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-vagrant.git
-git submodule add -b master ssh://git@stash.neofonie.de:7999/~monnin/ansible-role-dev_setup.git
 git submodule init
